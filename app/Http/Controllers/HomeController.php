656f4d57e82c1f3cb09b3c57f96491fed1a16b2b
mbahgembrong.sos@gmail.com <mbah gembrong>
2023-06-20 03:42:45 +0700

    add home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bantuan;
+use App\Models\Kriteria;
+use App\Models\Siswa;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +26,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $count = [];
+        $count['kriteria'] = Kriteria::count();
+        $count['siswa'] = Siswa::count();
+        $count['bantuan_proses'] = Bantuan::where('status', 'proses')->count();
+        $count['bantuan_selesai'] = Bantuan::where('status', 'selesai')->count();
+        return view('home', compact('count'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,75 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Home</h1>
+                </div>
+            </div>
+        </div>
+    </section>
 
+    <div class="content px-3">
+
+        @include('flash::message')
+
+        <div class="clearfix"></div>
+
+        <div class="row">
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>{{ $count['kriteria'] }}</h3>
+                        <p>Kriteria</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-bag"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ $count['siswa'] }}</h3>
+                        <p>Siswa</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{ $count['bantuan_proses'] }}</h3>
+                        <p>Bantuan Proses</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ $count['bantuan_selesai'] }}</h3>
+                        <p>Bantuan Selesai</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-pie-graph"></i>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
-</div>
 @endsection
